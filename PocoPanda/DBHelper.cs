43ba1863945364a0a73a2b0df9febeeff769c8a1
Rely Ky <relyky@hotmail.com>
2024-04-26 12:00:31 +0800

    GenerateTablePocoCode - table

---
+++ PocoPanda/DBHelper.cs
@@ -0,0 +1,168 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocoPanda;
+
+class DBHelper
+{
+  /// <summary>
+  /// SQL data type → NET data type
+  /// </summary>
+  public static string MapNetDataType(string sqlDataTypeName)
+  {
+    switch (sqlDataTypeName.ToLower())
+    {
+      case "bigint":
+        return "Int64";
+      case "binary":
+        return "Byte[]";
+      case "bit":
+        return "bool";
+      //case "char":
+      case string t when t.StartsWith("char"):
+        return "string";
+      case "cursor":
+        return string.Empty;
+      case "datetime":
+        return "DateTime";
+      case "datetime2":
+        return "DateTime";
+      case "decimal":
+        return "Decimal";
+      case "float":
+        return "Double";
+      case "int":
+        return "int";
+      case "money":
+        return "Decimal";
+      //case "nchar":
+      case string t when t.StartsWith("nchar"):
+        return "string";
+      case "numeric":
+        return "Decimal";
+      //case "nvarchar":
+      case string t when t.StartsWith("nvarchar"):
+        return "string";
+      case "real":
+        return "single";
+      case "smallint":
+        return "Int16";
+      case "text":
+        return "string";
+      case "tinyint":
+        return "Byte";
+      case "varbinary":
+        return "Byte[]";
+      case "xml":
+        return "string";
+      //case "varchar":
+      case string t when t.StartsWith("varchar"):
+        return "string";
+      case "smalldatetime":
+        return "DateTime";
+      case "image":
+        return "byte[]";
+      case "uniqueidentifier":
+        return "Guid";
+      case "datetimeoffset":
+        return "DateTimeOffset";
+      default:
+        return $"{sqlDataTypeName}:not_support"; // not support
+    }
+  }
+
+  public static List<TableInfo> LoadTable(SqlConnection conn)
+  {
+    string sql = @"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME != 'sysdiagrams' ";
+    var tableList = conn.Query<TableInfo>(sql).ToList();
+    return tableList;
+  }
+
+  public static List<ColumnInfo> LoadTableColumn(SqlConnection conn, string tableName, string tableSchema = "dbo")
+  {
+    string sql = $@"WITH PK AS (
+SELECT TC.CONSTRAINT_CATALOG,TC.CONSTRAINT_SCHEMA,TC.CONSTRAINT_NAME,TC.TABLE_NAME,TC.CONSTRAINT_TYPE, KC.COLUMN_NAME
+FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS TC
+INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE KC ON 
+	TC.CONSTRAINT_CATALOG = KC.CONSTRAINT_CATALOG AND 
+	TC.CONSTRAINT_SCHEMA = KC.CONSTRAINT_SCHEMA AND
+	TC.CONSTRAINT_NAME = KC.CONSTRAINT_NAME
+WHERE TC.CONSTRAINT_TYPE = 'PRIMARY KEY'
+)
+, MSDESC AS (
+select ss.name [TABLE_SCHEMA], st.name [TABLE_NAME], sc.name [COLUMN_NAME], sep.value [MS_Description]
+from sys.tables st
+inner join sys.columns sc on st.object_id = sc.object_id
+inner join sys.schemas ss on st.schema_id = ss.schema_id
+inner join sys.extended_properties sep on 
+  st.object_id = sep.major_id and 
+  sc.column_id = sep.minor_id and 
+  sep.name = 'MS_Description' and 
+  sep.value is not null
+)
+, COMPUTED AS (
+select ss.name [TABLE_SCHEMA], st.name [TABLE_NAME], sc.name [COLUMN_NAME]
+, sc.is_computed [IS_COMPUTED]
+, sc.definition [COMPUTED_DEFINITION]
+from sys.tables st
+inner join sys.computed_columns sc on st.object_id = sc.object_id
+inner join sys.schemas ss on st.schema_id = ss.schema_id
+where sc.is_computed = 1
+)
+SELECT C.COLUMN_NAME, C.ORDINAL_POSITION, C.TABLE_CATALOG, C.TABLE_SCHEMA, C.TABLE_NAME, C.IS_NULLABLE, C.DATA_TYPE, C.CHARACTER_MAXIMUM_LENGTH, C.COLUMN_DEFAULT
+, IS_IDENTITY = CASE WHEN COLUMNPROPERTY(object_id(C.TABLE_SCHEMA+'.'+C.TABLE_NAME), C.COLUMN_NAME, 'IsIdentity') = 1 THEN 'YES' ELSE 'NO' END
+, IS_PK = CASE WHEN PK.CONSTRAINT_TYPE = 'PRIMARY KEY' THEN 'YES' ELSE 'NO' END
+, MS_Description = MSDESC.MS_Description
+, IS_COMPUTED = CASE WHEN COMPUTED.IS_COMPUTED = 1 THEN 'YES' ELSE 'NO' END
+, COMPUTED.COMPUTED_DEFINITION
+FROM INFORMATION_SCHEMA.COLUMNS C
+LEFT JOIN PK ON C.COLUMN_NAME = PK.COLUMN_NAME AND
+  C.TABLE_NAME = PK.TABLE_NAME AND
+  C.TABLE_SCHEMA = PK.CONSTRAINT_SCHEMA AND
+  C.TABLE_CATALOG = PK.CONSTRAINT_CATALOG
+LEFT JOIN MSDESC ON C.COLUMN_NAME = MSDESC.COLUMN_NAME AND
+  C.TABLE_NAME = MSDESC.TABLE_NAME AND
+  C.TABLE_SCHEMA = MSDESC.TABLE_SCHEMA
+LEFT JOIN COMPUTED ON C.COLUMN_NAME = COMPUTED.COLUMN_NAME AND
+  C.TABLE_NAME = COMPUTED.TABLE_NAME AND
+  C.TABLE_SCHEMA = COMPUTED.TABLE_SCHEMA
+WHERE C.TABLE_NAME = @tableName
+ AND C.TABLE_SCHEMA = @tableSchema
+ORDER BY TABLE_NAME, ORDINAL_POSITION ASC ";
+
+    var columnList = conn.Query<ColumnInfo>(sql, new { tableSchema, tableName }).ToList();
+    return columnList;
+  }
+
+}
+
+record TableInfo
+{
+  public string TABLE_CATALOG { get; set; } = default!;
+  public string TABLE_SCHEMA { get; set; } = default!;
+  public string TABLE_NAME { get; set; } = default!;
+  public string TABLE_TYPE { get; set; } = default!;
+}
+
+record ColumnInfo
+{
+  public string COLUMN_NAME { get; set; } = default!;
+  public string ORDINAL_POSITION { get; set; } = default!;
+  public string TABLE_CATALOG { get; set; } = default!;
+  public string TABLE_SCHEMA { get; set; } = default!;
+  public string TABLE_NAME { get; set; } = default!;
+  public string DATA_TYPE { get; set; } = default!;
+  public string? CHARACTER_MAXIMUM_LENGTH { get; set; }
+  public string? COLUMN_DEFAULT { get; set; }
+  public string IS_NULLABLE { get; set; } = default!;
+  public string IS_IDENTITY { get; set; } = default!;
+  public string IS_PK { get; set; } = default!;
+  public string? MS_Description { get; set; }
+  public string IS_COMPUTED { get; set; } = default!;
+  public string? COMPUTED_DEFINITION { get; set; }
+}
--- PocoPanda/MainCommand.cs
+++ PocoPanda/MainCommand.cs
@@ -3,6 +3,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
@@ -75,8 +76,113 @@ class MainCommand
 
   void GenerateTablePocoCode(SqlConnection conn, DirectoryInfo outDir)
   {
+    Console.WriteLine("================================================================================");
     Console.WriteLine($"#BEGIN {nameof(GenerateTablePocoCode)}");
+    var tableList = DBHelper.LoadTable(conn);
+    tableList.ForEach(table =>
+    {
+      Console.WriteLine("--------------------------------------------------------------------------------");
+      Console.WriteLine($"#{table.TABLE_TYPE}: {table.TABLE_CATALOG}.{table.TABLE_SCHEMA}.{table.TABLE_NAME}");
+      StringBuilder pocoCode = new StringBuilder();
 
+      pocoCode.AppendLine($"namespace {_nameSpace}");
+      pocoCode.AppendLine("{");
+      pocoCode.AppendLine("using System;");
+      pocoCode.AppendLine("using System.ComponentModel.DataAnnotations;");
+      pocoCode.AppendLine("using System.ComponentModel.DataAnnotations.Schema;");
+      pocoCode.AppendLine();
 
+      pocoCode.AppendLine($"[Table(\"{table.TABLE_NAME}\")]");
+      pocoCode.AppendLine($"public class {table.TABLE_NAME} ");
+      pocoCode.AppendLine("{");
+      //---------------------
+      var columnList = DBHelper.LoadTableColumn(conn, table.TABLE_NAME, table.TABLE_SCHEMA);
+      columnList.ForEach(col =>
+      {
+        string dataType = DBHelper.MapNetDataType(col.DATA_TYPE);
+        bool isPrimaryKey = col.IS_PK == "YES";
+        bool isIdentity = col.IS_IDENTITY == "YES";
+        bool isComputed = col.IS_COMPUTED == "YES";
+        string nullable = (dataType != "string" && !isPrimaryKey) ? "?" : string.Empty; // ORM 欄位原則上都是 nullable 不然在 input binding 會很難實作。
+        string description = col.MS_Description ?? string.Empty;
+
+        //# summary
+        if (col.MS_Description != null || col.COMPUTED_DEFINITION != null)
+        {
+          pocoCode.AppendLine($"{_indent}/// <summary>");
+          if (col.MS_Description != null)
+            pocoCode.AppendLine($"{_indent}/// {col.MS_Description}");
+          if (col.COMPUTED_DEFINITION != null)
+            pocoCode.AppendLine($"{_indent}/// Computed Definition: {col.COMPUTED_DEFINITION}");
+          pocoCode.AppendLine($"{_indent}/// </summary>");
+        }
+
+        //# Display(Name) := 欄位名稱:欄位說明欄位說明欄位說明欄位說明。
+        if (!String.IsNullOrWhiteSpace(col.MS_Description))
+        {
+          string displayName = col.MS_Description.Split(':', '：', '\r', '\n')[0].Trim();
+          pocoCode.AppendLine($"{_indent}[Display(Name = \"{displayName}\")]");
+        }
+
+        //# Key & Computed attribute
+        if (isPrimaryKey)
+          pocoCode.AppendLine($"{_indent}[Key]");
+        if (isIdentity)
+          pocoCode.AppendLine($"{_indent}[DatabaseGenerated(DatabaseGeneratedOption.Identity)]");
+        if (isComputed)
+          pocoCode.AppendLine($"{_indent}[DatabaseGenerated(DatabaseGeneratedOption.Computed)]");
+
+        //# Required attribute
+        if (col.IS_NULLABLE == "NO")
+          pocoCode.AppendLine($"{_indent}[Required]");
+
+        //# filed
+        pocoCode.AppendLine($"{_indent}public {dataType}{nullable} {col.COLUMN_NAME} {{ get; set; }}");
+      });
+
+      //## 產生Copy函式
+      //public void Copy(職員基本表 src)
+      //{
+      //    this.職員代碼 = src.職員代碼;
+      //    this.職員姓名 = src.職員姓名;
+      //    this.狀態 = src.狀態;
+      //}
+
+      pocoCode.AppendLine();
+      pocoCode.AppendLine($"{_indent}public void Copy({table.TABLE_NAME} src)");
+      pocoCode.AppendLine($"{_indent}{{");
+      columnList.ForEach(col =>
+                pocoCode.AppendLine($"{_indent}{_indent}this.{col.COLUMN_NAME} = src.{col.COLUMN_NAME};"));
+      pocoCode.AppendLine($"{_indent}}}"); // end of: Copy
+
+      //## 產生Clone函式
+      //public 職員基本表 Clone()
+      //{
+      //    return new 職員基本表
+      //    {
+      //        職員代碼 = this.職員代碼,
+      //        職員姓名 = this.職員姓名,
+      //        狀態 = this.狀態
+      //    };
+      //}
+
+      pocoCode.AppendLine();
+      pocoCode.AppendLine($"{_indent}public {table.TABLE_NAME} Clone()");
+      pocoCode.AppendLine($"{_indent}{{");
+      pocoCode.AppendLine($"{_indent}{_indent}return new {table.TABLE_NAME} {{");
+      columnList.ForEach(col =>
+                pocoCode.AppendLine($"{_indent}{_indent}{_indent}{col.COLUMN_NAME} = this.{col.COLUMN_NAME},"));
+      pocoCode.AppendLine($"{_indent}{_indent}}};");
+      pocoCode.AppendLine($"{_indent}}}"); // end of: Clone
+
+      //---------------------
+      pocoCode.AppendLine("}"); // end of: Class
+      pocoCode.AppendLine("}"); // end of: Namespace
+      pocoCode.AppendLine();
+
+      //## 一個 Table 一個檔案
+      File.WriteAllText(Path.Combine(outDir.FullName, $"{table.TABLE_NAME}.cs"), pocoCode.ToString(), encoding: Encoding.UTF8);
+      Console.WriteLine(pocoCode.ToString());
+    });
   }
 }
