d43f4d63f7735afc79092ac9532f3f219d10c938
Rely Ky <relyky@hotmail.com>
2024-04-26 11:29:20 +0800

    Command - init

--- PocoPanda/MainCommand.cs
+++ PocoPanda/MainCommand.cs
@@ -2,20 +2,51 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PocoPanda;
 
-class MainCommand(IConfiguration _config)
+/// <summary>
+/// 主要命令
+/// </summary>
+class MainCommand
 {
-  [Command(Description = "SQL Server POCO tool。")]
+  //# 參數
+  readonly string _connStr;
+  readonly string _nameSpace;
+  readonly string _outputFolder;
+  readonly string _indent = "  ";
+  readonly string _sqlClientLibrary = "Microsoft.Data.SqlClient";
+  readonly bool _exportExcel;
+
+  [RequiresUnreferencedCode("Calls Microsoft.Extensions.Configuration.ConfigurationBinder.GetValue<T>(String, T)")]
+  public MainCommand(IConfiguration config)
+  {
+    //# 取得參數
+    _connStr = config.GetConnectionString("DefaultConnection");
+    _nameSpace = config["Namespace"];
+    _outputFolder = config["OutputFolder"];
+    _exportExcel = config.GetValue<bool>("ExportExcel", false);
+  }
+
+  [Command(Description = "SQL Server POCO tool。所有參數均填入 appsettings.json。")]
   public Task CommandProcedure()
   {
     Console.WriteLine($"#BEGIN {nameof(MainCommand)}");
+    Console.WriteLine($"§ 參數");
+    Console.WriteLine($"連線字串：{_connStr}");
+    Console.WriteLine($"輸出目錄：{_outputFolder}");
+    Console.WriteLine($"命名空間：{_nameSpace}");
+    Console.WriteLine($"SqlClient 套件：{_sqlClientLibrary}");
+    Console.WriteLine($"產生 Excel：{_exportExcel}");
+    Console.WriteLine();
 
-    Console.WriteLine($"OutputFolder: {_config["OutputFolder"]}");
+
     return Task.CompletedTask;
   }
+
+
 }
--- PocoPanda/Program.cs
+++ PocoPanda/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PocoPanda;
 using PocoPanda.Services;
+using Dapper;
+
+[module: DapperAot(true)]
 
 IConfiguration config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -16,8 +19,5 @@ builder.Services.AddScoped<RandomService>();
 var app = builder.Build();
 
 app.AddCommands<MainCommand>();
-
-//// Command 指令。Class-based style。
-//app.AddCommands<GreetingCommand>();
 
 app.Run();
